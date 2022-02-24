e2951de49c815d6cbe2c5213ad78779b89efdc82
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-02-24 11:07:56 -0500

    Add project files.

---
+++ SalesMVC/Data/SalesMVCContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesMVC.Models;
+
+namespace SalesMVC.Data
+{
+    public class SalesMVCContext : DbContext
+    {
+
+        public DbSet<Customer> Customers { get; set; }
+        public SalesMVCContext (DbContextOptions<SalesMVCContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder) { }
+        
+    }
+}
